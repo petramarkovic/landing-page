7b09165e1b81d82d65a82dcf48865fd46abc5763
petramarkovic <petramarkovic996@gmail.com>
2023-11-18 20:46:33 +0100

    Add logos module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.scss';
 import './assets/images/logo.svg';
 import './assets/images/banner-image.jpg';
+import './assets/images/logos-image-1.svg';
+import './assets/images/logos-image-2.svg';
+import './assets/images/logos-image-3.svg';
+import './assets/images/logos-image-4.svg';
+import './assets/images/logos-image-5.svg';
 
 import init from './js/init';
 init.init();
